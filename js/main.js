1759d452e11859888a677b9a65b2823ad859ba36
Yanela <yanelalzn05@gmail.com>
2025-04-14 15:26:45 -0500

    avance

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
-const buttons = document.querySelectorAll('button');
-var header = document.getElementById('Header');
+// const buttons = document.querySelectorAll('button');
+// var header = document.getElementById('Header');
 
-buttons.forEach( button =>{
-    button.addEventListener('click',()=>{
-        const faq = button.nextElementSibling;
-        const icon = button.children[1];
+// buttons.forEach( button =>{
+//     button.addEventListener('click',()=>{
+//         const faq = button.nextElementSibling;
+//         const icon = button.children[1];
 
-        faq.classList.toggle('show');
-        icon.classList.toggle('rotate');
-    })
-} )
+//         faq.classList.toggle('show');
+//         icon.classList.toggle('rotate');
+//     })
+// } )
 
 
-window.addEventListener('scroll', ()=> {
-    var scroll = window.scrollY
+// window.addEventListener('scroll', ()=> {
+//     var scroll = window.scrollY
 
-    if(scroll>4){
-        header.style.backgroundColor = 'rgba(0,0,0,0.17)'
-        header.style.padding = ' 1rem 5rem 0 4rem'
-        // header.style.top = '0'
-    }else{
-        header.style.backgroundColor = ''
-        header.style.padding = ' 3rem 5rem 0 4rem'
+//     if(scroll>4){
+//         header.style.backgroundColor = 'rgba(0,0,0,0.17)'
+//         header.style.padding = ' 1rem 5rem 0 4rem'
+//         // header.style.top = '0'
+//     }else{
+//         header.style.backgroundColor = ''
+//         header.style.padding = ' 3rem 5rem 0 4rem'
 
-    }
+//     }
+// })
+
+
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
 })
